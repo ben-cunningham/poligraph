28770e9fb06db84ec4d2a180feccffee5a9e526b
ben-cunningham <bencunningham17@gmail.com>
2017-04-16 21:04:12 -0700

    Add webpack necessities

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+var path = require('path');
+var webpack = require('webpack');
+ 
+module.exports = {
+    entry: './server/static/main.js',
+    output: { path: __dirname +'server/templates/', filename: 'bundle.js' },
+    module: {
+        loaders: [
+            {
+                test: /.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    },
+};
