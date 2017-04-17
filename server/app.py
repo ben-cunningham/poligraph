d06eec860b0396ac513a222543982c3e9f453a4b
ben-cunningham <bencunningham17@gmail.com>
2017-04-16 20:22:08 -0700

    Add api route

--- server/app.py
+++ server/app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/api')
+def api():
+    return 'Hello friends'
+
 @app.route('/')
 def index():
     return render_template('index.html')
