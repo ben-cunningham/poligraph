91f44f4cca092195d8c9d72c47b63709d20535ca
ben-cunningham <bencunningham17@gmail.com>
2017-04-17 21:32:16 -0700

    Add parameter handling

--- server/app.py
+++ server/app.py
@@ -1,9 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/api')
 def api():
+    if 'src' not in request.args:
+        return 'src node not provided', 400
+    if 'dest' not in request.args:
+        return 'dest node not provided', 400
+    
+    src = request.args.get('src')
+    dest = request.args.get('dest')
+
+
+
     return 'Hello friends'
 
 @app.route('/')
