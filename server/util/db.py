f65047d8b99f5d9802315dcc35bca86ab40da08f
ben-cunningham <bencunningham17@gmail.com>
2017-06-11 10:51:31 -0700

    Implemnent edge searching for db

--- server/util/db.py
+++ server/util/db.py
@@ -1,7 +1,28 @@
+import psycopg2
+import configparser
+from urllib.parse import urlparse
+
+
 class DatabaseManager():
     
     def __init__(self):
-        pass
+        config = configparser.ConfigParser()
+        config.read('config.ini')
+        url = config.get('Database config', 'DATABASE_URL')
+        url = urlparse(url)
 
-    def get_edges(node):
-        pass
+        self.conn = psycopg2.connect(
+            database=url.path[1:],
+            user=url.username,
+            password=url.password,
+            host=url.hostname,
+            port=url.port
+        )
+
+    def get_edges(self, node):
+        cur = self.conn.cursor()
+        cur.execute(""" select * from edge where "to"=%s; """, (node, ))
+        rows = [row for row in cur]
+        cur.close()
+        return rows
+
