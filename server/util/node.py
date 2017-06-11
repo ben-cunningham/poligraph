20cbfe95a4e0e53c9a7b6c5ed3260d319bf95879
ben-cunningham <bencunningham17@gmail.com>
2017-06-11 14:51:02 -0700

    Add path class for graph edges

--- server/util/node.py
+++ server/util/node.py
@@ -1,5 +1,12 @@
+class Path():
+    
+    def __init__(self, node, context):
+        self.node = node
+        self.context = context
+
 class Node():
     
     def __init__(self, id_):
         self.id_ = id_
-        self.edges = []
+        self.paths = []
+
