f75a8c1fc1b799b9ec049577c2a77bf9b2a0b240
ben-cunningham <bencunningham17@gmail.com>
2017-06-11 14:31:55 -0700

    Add comment for TODO

--- server/util/db_util.py
+++ server/util/db_util.py
@@ -1,7 +1,9 @@
 from util.db import DatabaseManager
 
 class DatabaseUtil():
-    
+    """
+    TODO: Make these static methods
+    """
     def get_edges(self, src):
         edges = DatabaseManager().get_edges(src)
         print(len(edges))
