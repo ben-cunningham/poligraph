5e3a3fbe6e49bb87c1316acc2bb2c1574754f2bf
ben-cunningham <bencunningham17@gmail.com>
2017-06-11 15:03:09 -0700

    Add methods to index dictionary by node

--- server/util/graph.py
+++ server/util/graph.py
@@ -3,6 +3,12 @@ class Node():
     def __init__(self, id_):
         self.id_ = id_
         self.name = ""
+    
+    def __hash__(self):
+        return hash(self.id_)
+
+    def __eq__(self, other):
+        return self.id_ == other.id_
 
 class Graph():
     
