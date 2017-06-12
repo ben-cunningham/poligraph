aef119227037dd83a5ed86eb39a8c6284c3afbef
ben-cunningham <bencunningham17@gmail.com>
2017-06-11 22:41:12 -0700

    Add each connection to the graph given a node

--- server/util/db_util.py
+++ server/util/db_util.py
@@ -1,9 +1,12 @@
 from util.db import DatabaseManager
+from util.graph import Node
 
 class DatabaseUtil():
     """
     TODO: Make these static methods
     """
-    def get_edges(self, src):
-        edges = DatabaseManager().get_edges(src)
-        print(len(edges))
+    def get_edges(self, src, graph):
+        edges = DatabaseManager().get_edges(src.id_)
+        for edge in edges:
+            graph.add_connection(src, dest)
+            
