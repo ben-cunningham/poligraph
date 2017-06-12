ab7df56aa076e7a43675c56709594cabfec7779b
ben-cunningham <bencunningham17@gmail.com>
2017-06-11 22:41:45 -0700

    Start implementation of breadth first search

--- server/util/search.py
+++ server/util/search.py
@@ -1,11 +1,24 @@
 from util.db_util import DatabaseUtil
-from util.node import Node
+from util.graph import Node, Graph
 
 class Search():
 
     def __init__(self, src):
-         self.src = self.Node(src)
+         self.src = Node(src)
+         self.g = Graph()
+         self.g.add_vertex(self.src)
 
     def bfs(self, dest):
-         dest = self.Node(dest)
-         DatabaseUtil().get_edges(dest.id_)
+         dest = Node(dest)
+         path = [self.src]
+         if self.src == dest:
+             return path
+
+         queue = [self.src]
+         while len(queue) > 0:
+             node = queue.pop(0)
+             self.get_edges(node)
+
+    def get_edges(self, node):
+        edges = DatabaseUtil().get_edges(node, self.g)
+
