325fc9a637a2b8a97c5520d3825d27c2d6752589
ben-cunningham <bencunningham17@gmail.com>
2017-06-15 20:33:07 -0700

    Retain path for search

--- server/util/search.py
+++ server/util/search.py
@@ -10,20 +10,21 @@ class Search():
 
     def bfs(self, dest):
          dest = Node(dest)
-         path = []
+         path = [[self.src]]
          visited = set()
-         queue = [self.src]
-         
-         while len(queue) > 0:
-             node = queue.pop(0)
+
+         while len(path) > 0:
+             curr_path = path.pop(0)
+             node = curr_path[-1]
              if node == dest:
-                 path.append(node)
-                 return path
+                 return curr_path
 
              self.get_edges(node) # get the neighboring nodes from the database and populate graph
              for v in self.g.get_adjacent_verticies(node):
                  if v not in visited:
-                     queue.append(v)
+                     new_path = list(curr_path)
+                     new_path.append(v)
+                     path.append(new_path)
                      visited.add(v)
 
     def get_edges(self, node):
