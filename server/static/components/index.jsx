ccc91c30b7cde3fd84095c67496d2ad471f1a4ae
ben-cunningham <bencunningham17@gmail.com>
2017-06-15 21:35:15 -0700

    Call api from main page

--- server/static/components/index.jsx
+++ server/static/components/index.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import ApiClient from '../services/api.js';
+
 class Index extends React.Component {
+  componentDidMount() {
+      var client = new ApiClient();
+      client.getPath("Q6294", "Q76");
+  }
+
   render() {
     return <h1> Connect-DC </h1>
   }
