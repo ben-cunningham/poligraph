f88911467ee92ed06de49f51ce2b8a7827e80120
ben-cunningham <bencunningham17@gmail.com>
2017-06-17 12:59:18 -0700

    Implement basic Results classes

---
+++ server/static/components/Results.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import ResultsCell from './ResultsCell.jsx';
+
+class Results extends React.Component {
+    
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
+
+export default Results;
