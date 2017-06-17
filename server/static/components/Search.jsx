695acd1f635b302dd9af4563f028e1b6394d9671
ben-cunningham <bencunningham17@gmail.com>
2017-06-17 12:46:17 -0700

    Move search API call to submit button

--- server/static/components/Search.jsx
+++ server/static/components/Search.jsx
@@ -2,12 +2,20 @@ import React from 'react';
 
 import SearchBar from './SearchBar.jsx';
 
+import ApiClient from '../services/api.js';
+
 class Search extends React.Component {
+    handleSearch() {
+        var client = new ApiClient();
+        client.getPath("Q6294", "Q76");
+    }
+    
     render() {
         return (
             <div>
                 <SearchBar></SearchBar>
                 <SearchBar></SearchBar>
+                <button type="button" onClick={this.handleSearch}>Search</button>
             </div>
         )
     }
--- server/static/components/index.jsx
+++ server/static/components/index.jsx
@@ -3,13 +3,7 @@ import ReactDOM from 'react-dom';
 
 import Search from './Search.jsx';
 
-import ApiClient from '../services/api.js';
-
 class Index extends React.Component {
-  componentDidMount() {
-      var client = new ApiClient();
-      client.getPath("Q6294", "Q76");
-  }
 
   render() {
     return (
