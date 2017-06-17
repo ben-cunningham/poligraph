222710c6354592d25c7a72d440eeec2a051cef24
ben-cunningham <bencunningham17@gmail.com>
2017-06-17 12:17:40 -0700

    Add component for handling search

---
+++ server/static/components/Search.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import SearchBar from './SearchBar.jsx';
+
+class Search extends React.Component {
+    render() {
+        return (
+            <div>
+                <SearchBar></SearchBar>
+                <SearchBar></SearchBar>
+            </div>
+        )
+    }
+}
+
+export default Search;
