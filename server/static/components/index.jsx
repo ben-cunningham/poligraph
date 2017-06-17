7b4236e55a633a1dcff31da9e44d1466237f4107
ben-cunningham <bencunningham17@gmail.com>
2017-06-17 14:04:46 -0700

    Pass search results to Results component

--- server/static/components/index.jsx
+++ server/static/components/index.jsx
@@ -5,14 +5,27 @@ import Search from './Search.jsx';
 import Results from './Results.jsx';
 
 class Index extends React.Component {
+  
+  constructor() {
+      super(props);
+      this.state = {
+          path: []
+      };
+  }
+
+  onFinishedSearch(data) {
+    this.setState({
+      path: data
+    });
+  }
 
   render() {
     return (
-        <div>
-            <h1> Connect-DC </h1>
-            <Search></Search>
-            <Results></Results>
-        </div>
+      <div>
+        <h1> Connect-DC </h1>
+          <Search onFinishedSearch={this.onFinishedSearch}></Search>
+          <Results path={this.state.path}></Results>
+      </div>
     )
   }
 }
