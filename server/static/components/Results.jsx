f55e134b38f0cf372e324ca13b9b18b9431dad78
ben-cunningham <bencunningham17@gmail.com>
2017-06-18 15:33:49 -0700

    Render each cell with props

--- server/static/components/Results.jsx
+++ server/static/components/Results.jsx
@@ -3,10 +3,19 @@ import React from 'react';
 import ResultsCell from './ResultsCell.jsx';
 
 class Results extends React.Component {
-    
+
     render() {
+        var rows = [];
+        this.props.path.forEach((segment) => {
+            rows.push(<ResultsCell key={segment.id}
+                                   src={segment.id}
+                                   name={segment.name} />);
+        });
+
         return (
-            <div></div>
+            <div>
+                {rows}
+            </div>
         )
     }
 }
