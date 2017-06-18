c5d42a7e75b20717b55ba7502173cf7f2366ad22
ben-cunningham <bencunningham17@gmail.com>
2017-06-18 15:34:18 -0700

    Fix 'this' binding bug

--- server/static/components/Search.jsx
+++ server/static/components/Search.jsx
@@ -5,11 +5,15 @@ import SearchBar from './SearchBar.jsx';
 import ApiClient from '../services/api.js';
 
 class Search extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.handleSearch = this.handleSearch.bind(this);
+    }
+    
     handleSearch() {
         var client = new ApiClient();
-        client.getPath("Q6294", "Q76", function(data) {
-            this.props.OnFinishedSearch(data);
-        });
+        client.getPath("Q6294", "Q76", (data) => this.props.onFinishedSearch(data));
     }
     
     render() {
