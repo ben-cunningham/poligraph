2ec44e4ecb127c19915ece3546c34f22944a2738
ben-cunningham <bencunningham17@gmail.com>
2017-06-18 16:12:14 -0700

    Move search to graph class and add a manager

---
+++ server/util/connections.py
@@ -0,0 +1,15 @@
+from util.db import DatabaseManager
+from util.graph import Graph, Node
+
+class Connections():
+
+    def __init__(self, src):
+        self.g = Graph(src, self)
+
+    def search(self, dest):
+        return self.g.bfs(dest)
+
+    def get_edges(self, src):
+        edges = DatabaseManager().get_edges(src.id_)
+        for edge in edges:
+            self.g.add_connection(src, Node(edge[1]), "")
--- server/util/graph.py
+++ server/util/graph.py
@@ -18,8 +18,11 @@ class Node():
 
 class Graph():
     
-    def __init__(self):
+    def __init__(self, src, manager):
+        self.manager = manager
         self.verticies = {}
+        self.src = Node(src)
+        self.add_vertex(self.src)
 
     def add_vertex(self, node):
         self.verticies[node] = []
@@ -35,4 +38,23 @@ class Graph():
         if src not in self.verticies:
             raise Exception
         return self.verticies[src]
+
+    def bfs(self, dest):
+         dest = Node(dest)
+         path = [[self.src]]
+         visited = set()
+
+         while len(path) > 0:
+             curr_path = path.pop(0)
+             node = curr_path[-1]
+             if node == dest:
+                 return curr_path
+
+             self.manager.get_edges(node) # get the neighboring nodes from the database and populate graph
+             for v in self.get_adjacent_verticies(node):
+                 if v not in visited:
+                     new_path = list(curr_path)
+                     new_path.append(v)
+                     path.append(new_path)
+                     visited.add(v)
 
