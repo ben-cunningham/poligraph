888734a188b0d6d852f3f0085a5de121e4201348
ben-cunningham <bencunningham17@gmail.com>
2017-06-18 17:00:59 -0700

    Switch to SearchContainer

--- server/static/components/Search.jsx
+++ server/static/components/Search.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import SearchBar from './SearchBar.jsx';
+import SearchContainer from './SearchContainer.jsx';
 
 import ApiClient from '../services/api.js';
 
@@ -9,18 +9,23 @@ class Search extends React.Component {
     constructor(props) {
         super(props);
         this.handleSearch = this.handleSearch.bind(this);
+        this.onSelection = this.onSelection.bind(this);
     }
     
     handleSearch() {
         var client = new ApiClient();
         client.getPath("Q6294", "Q76", (data) => this.props.onFinishedSearch(data));
     }
+
+    onSelection() {
+    
+    }
     
     render() {
         return (
             <div>
-                <SearchBar onSelection={this.handleSelection}></SearchBar>
-                <SearchBar onSelection={this.handleSelection}></SearchBar>
+                <SearchContainer onSelection={this.handleSelection} />
+                <SearchContainer onSelection={this.handleSelection} />
                 <button type="button" onClick={this.handleSearch}>Search</button>
             </div>
         )
