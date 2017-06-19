e2c74bb47cf9e1059b05db861e16b7f1e14cc7fc
ben-cunningham <bencunningham17@gmail.com>
2017-06-18 17:14:56 -0700

    Hook up callbacks for dropdown selection

--- server/static/components/SearchContainer.jsx
+++ server/static/components/SearchContainer.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import ApiClient from '../services/api.js';
+
 import SearchBar from './SearchBar.jsx';
 import SearchDropDown from './SearchDropDown.jsx';
 
@@ -7,17 +9,25 @@ class SearchContainer extends React.Component {
   constructor(props) {
     super(props);
     this.onInputChange = this.onInputChange.bind(this);
+    this.onSelection = this.onSelection.bind(this);
   }
 
-  onInputChange() {
-  
+  onInputChange(input) {
+    var client = new ApiClient();
+    client.SearchPolitician(input, function(results) {
+      this.setState(results);
+    });
+  }
+
+  onSelection(selection) {
+    this.props.onSelection(selection);
   }
   
   render() {
     return (
       <div class="search-container">
         <SearchBar onInputChange={this.onInputChange} />
-        <SearchDropDown results={this.searchResults} />
+        <SearchDropDown results={this.state.searchResults} onSelection={this.onSelection}/>
       </div>
     );
   }
