c8a2a2409a6525a10d54049bea93a424ce62a03d
ben-cunningham <bencunningham17@gmail.com>
2017-06-22 20:36:35 -0700

    Change class to be for context

---
+++ server/static/components/ContextResultsCell.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class ContextResultsCell extends React.Component {
+    
+  render() {
+    return (
+      <div>
+        {this.props.src}
+      </div>
+    )
+  }
+}
+
+export default ContextResultsCell;
