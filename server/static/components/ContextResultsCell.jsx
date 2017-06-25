4c522b2f37fcb029ece4d42702075c3ec0aebed4
ben-cunningham <bencunningham17@gmail.com>
2017-06-25 16:04:46 -0700

    Render context as list

--- server/static/components/ContextResultsCell.jsx
+++ server/static/components/ContextResultsCell.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 
 class ContextResultsCell extends React.Component {
-    
+  
+  textToList() {
+    return this.props.text.split('|');
+  }
+
   render() {
+    var textList = this.textToList();
+    var ul = [];
+    textList.forEach((elem, i) => {
+      ul.push(<li key={i}>{elem}</li>);
+    });
     return (
       <div>
-        {this.props.text}
+        <ul>
+          {ul}
+        </ul>
       </div>
     )
   }
