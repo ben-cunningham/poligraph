2a77c8049c59b8307c5f91be2d2cd95798021786
ben-cunningham <bencunningham17@gmail.com>
2017-06-29 20:51:01 -0700

    Space out input boxes

--- server/static/components/SearchBar.jsx
+++ server/static/components/SearchBar.jsx
@@ -12,7 +12,7 @@ class SearchBar extends React.Component {
 
     render() {
         return (
-            <input type="text" onChange={this.handleChange}/>
+            <input type="text" className="search" onChange={this.handleChange}/>
         ) 
     }
 }
