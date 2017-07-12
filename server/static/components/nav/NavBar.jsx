03151bfde53b5641a273e0687b5071da55eead46
ben-cunningham <bencunningham17@gmail.com>
2017-07-11 21:49:08 -0700

    Add link to about page

--- server/static/components/nav/NavBar.jsx
+++ server/static/components/nav/NavBar.jsx
@@ -4,7 +4,10 @@ class NavBar extends React.Component {
   render() {
     return (
       <div className="navbar navbar-toggleable-md">
-        <span className="navbar-brand title"><b>Poligraph</b></span>
+        <span className="navbar-brand title">
+          <b>Poligraph</b>
+          <a href="/about"><i className="fa fa-question-circle" aria-hidden="true"></i></a>
+        </span>
       </div>
     );
   }
